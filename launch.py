d0e32ca3db2092d2be6dfddfcdb72b2966e9b9a8
mvb <>
2022-10-18 15:22:58 +0100

    command history

--- launch.py
+++ launch.py
@@ -3,4 +3,5 @@ from talon import app, actions
 def app_launch():
     actions.user.mouse_toggle_zoom_mouse()
     actions.speech.disable()
+    actions.user.history_toggle()
 app.register("launch", app_launch)
