3705ffbb83cbf0d58f570ed0ce1cef1f5f816e70
mvb <60481661+MeteorVsBunny@users.noreply.github.com>
2022-12-13 09:56:31 +0000

    zoom mouse on start up now remembered

--- launch.py
+++ launch.py
@@ -1,7 +1,7 @@
 from talon import app, actions
 
 def app_launch():
-    actions.user.mouse_toggle_zoom_mouse()
+    # actions.user.mouse_toggle_zoom_mouse()
     actions.speech.disable()
     actions.user.history_toggle()
 app.register("launch", app_launch)
