45a17d16a5d3bc707a9911c894d1c568468cca3e
mvb <60481661+MeteorVsBunny@users.noreply.github.com>
2023-01-30 18:20:42 +0000

    launch log

--- launch.py
+++ launch.py
@@ -4,4 +4,9 @@ def app_launch():
     # actions.user.mouse_toggle_zoom_mouse()
     actions.speech.disable()
     actions.user.history_toggle()
+    # open log on left monitor
+    actions.menu.open_log()
+    actions.sleep("1000ms")
+    actions.key("super-shift-left super-up")
+
 app.register("launch", app_launch)
